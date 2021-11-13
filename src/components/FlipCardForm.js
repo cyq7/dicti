a24dd520b518e953f4302dd3a20afc7dd871fdd7
Cezary Kocinski <ckocinski7@wp.pl>
2021-11-13 15:24:49 +0100

    implement dynamic textarea with select option

--- src/components/FlipCardForm.js
+++ src/components/FlipCardForm.js
@@ -1,23 +1,44 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './styles/FlipCardForm.scss'
 
 export default function FlipCardForm({currentDefinitions, word, displayForm, handleClose}) {
+    const [chosenDefinition, setChosenDefinition] = useState('');
+
+    useEffect(() => {
+        setChosenDefinition(currentDefinitions[0])
+    }, [currentDefinitions])
+
+    function handleChange(e) {
+        setChosenDefinition(e.target.value);
+    }
+
+    console.log(chosenDefinition)
+
+    function addFlipCard(e) {
+        e.preventDefault();
+        console.log('works');
+    }
+
     if(displayForm) {
         return (
             <div className="flip-card-container">
                 <button onClick={handleClose} className='close'>X</button>
-                <div className="flip-card-form">
+                <form onSubmit={addFlipCard} className="flip-card-form">
                     <span>{word}</span>
                     <p>Definition</p>
-                    <select> 
-                        {currentDefinitions.map(def => {
-                            return (
-                                <option className="select-option" key={def}>{def}</option>
-                            )
-                        })}
-                    </select>
-                    <button>Add flip card</button>
-                </div>
+                    <div className="select-wrapper">
+                        <select onChange={handleChange} className="select">
+                            {currentDefinitions.map(def => {
+                                return (
+                                    <option className="select-option" key={def}>{def}</option>
+                                )
+                            })}
+                        </select>
+                    </div>
+
+                    <textarea maxLength="200" defaultValue={chosenDefinition}></textarea>
+                    <button type="submit">Add flip card</button>
+                </form>
             </div>
         )
     } else { 
